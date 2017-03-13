4aa7587edd6c01e13804bf20fe2efadffb95bbe7
Dorel Vaida <dorel@eloquentix.com>
2017-03-13 08:12:44 +0200

    Add some pretty pictures

--- utils.py
+++ utils.py
@@ -8,9 +8,11 @@ import os
 try:
     import matplotlib
     import sys
+
     matplotlib.use('TkAgg' if 'darwin' in sys.platform else 'Agg')
     import matplotlib.pyplot as plt
 except Exception as ex:
+    print ex
     print("You'll have to install matplotlib if you want to save some pretty graphs")
 
 
@@ -24,16 +26,18 @@ def build_training_data():
 
     return X_train, y_train, X_test, y_test
 
+
 def callbacks():
     reduce_lr = ReduceLROnPlateau(factor=0.8, patience=3, min_lr=0.00001, verbose=1)
     return [reduce_lr]
+
 
 def plot_results(history, accuracy, title):
     graphs_dir = '_graphs'
     if not os.path.isdir(graphs_dir):
         os.mkdir(graphs_dir)
     try:
-        plt.figure(figsize=(12,10))
+        plt.figure(figsize=(12, 10))
         plt.subplot(311)
         plt.plot(history.history['acc'])
         plt.plot(history.history['val_acc'])
@@ -41,7 +45,11 @@ def plot_results(history, accuracy, title):
         plt.ylabel('Accuracy')
         plt.grid()
 
-        plt.title("{} Test Accuracy: {:.2f}%".format(title.upper(), accuracy*100))
+        max_val_acc = max(history.history['val_acc'])
+        plt.title("{} Accuracy: {:.2f}%, Max (rnd {}): {:.2f}%".format(
+            title.upper(), accuracy * 100, history.history['val_acc'].index(max_val_acc) + 1,
+                           max(history.history['val_acc']) * 100
+        ))
 
         plt.subplot(312)
         plt.plot(history.history['loss'])
@@ -58,5 +66,6 @@ def plot_results(history, accuracy, title):
         plt.grid()
 
         plt.savefig('{}/{}_mnist.png'.format(graphs_dir, re.sub('\W', '_', title)).lower())
-    except:
+    except Exception as ex:
+        print ex
         print("You'll have to install matplotlib if you want to save some pretty graphs")
